e18cf4802b89b0901bdcc3143a0b0be11c8f9f67
Camille CLAVIER <camilleclavier9@gmail.com>
2022-11-09 11:32:36 +0100

    ajout argument curl

--- traitement_url_base.sh
+++ traitement_url_base.sh
@@ -12,6 +12,13 @@
 fichier_urls=$1 # le fichier d'URL en entrée
 fichier_tableau=$2 # le fichier HTML en sortie
 
+#si fichier tableau n'existe pas
+if (! -f $2)
+then 
+	echo "fichier tableau non trouver";
+	exit
+fi
+
 # !!!!!!
 # ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
 # !!!!!!
@@ -23,6 +30,9 @@ lineno=1;
 
 while read -r line;
 do
+	curl=$(curl -sIL $line)
+	grep=$(grep "HTTP/1.1 200")
+	
 	echo "ligne $lineno: $line";
 	lineno=$((lineno+1));
 done < $fichier_urls
